a1d78ab7ace998de4ac933217d7b1f8a687d14cf
CocoaShu <m17621628570@163.com>
2017-10-31 20:06:56 +0800

    用户的完善

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,11 +1,7 @@
 /**
  * Created by acera on 2017/10/29.
  */
-<<<<<<< HEAD
 $(function() {
-=======
-$(function () {
->>>>>>> 3819f4e110fb35eadd93e45cb63a278fed1e340a
     var $form = $("#form");
     $form.bootstrapValidator({
         //1. 指定不校验的类型，默认为[':disabled', ':hidden', ':not(:visible)'],可以不设置
@@ -31,18 +27,13 @@ $(function () {
                     stringLength: {
                         min: 4,
                         max: 30,
-<<<<<<< HEAD
                         message: '用户名长度必须在到30之间'
-=======
-                        message: '用户名长度必须在4到30之间'
->>>>>>> 3819f4e110fb35eadd93e45cb63a278fed1e340a
                     },
                     //正则校验
                     regexp: {
                         regexp: /^[a-zA-Z0-9_\.]+$/,
                         message: '用户名由数字字母下划线和.组成'
                     },
-<<<<<<< HEAD
                     callback: {
                         message: '用户名错误'
                     }
@@ -50,15 +41,6 @@ $(function () {
             },
             password: {
                 validators: {
-=======
-                    callback:{
-                        message:'用户名错误'
-                    }
-                }
-            },
-            password:{
-                validators:{
->>>>>>> 3819f4e110fb35eadd93e45cb63a278fed1e340a
                     notEmpty: {
                         message: '密码不能为空'
                     },
@@ -73,18 +55,13 @@ $(function () {
                         regexp: /^[a-zA-Z0-9_\.]+$/,
                         message: '密码由数字字母下划线和.组成'
                     },
-<<<<<<< HEAD
                     callback: {
-=======
-                    callback:{
->>>>>>> 3819f4e110fb35eadd93e45cb63a278fed1e340a
                         message: '密码不正确'
                     }
                 }
             },
         }
 
-<<<<<<< HEAD
     }).on('success.form.bv', function(e) {
         e.preventDefault();
         $.ajax({
@@ -116,36 +93,3 @@ $(function () {
 
 
 });
-=======
-    }).on('success.form.bv',function (e) {
-        e.preventDefault();
-$.ajax({
-   type:'post',
-    url:'/employee/employeeLogin',
-    data:$form.serialize(),
-    dataType:'json',
-    success:function (data) {
-        if(data.success){
-            location.href='index.html';
-        }else{
-            if(data.error==1000){
-                $form.data('bootstrapValidator').updateStatus('username','INVALID','callback');
-            }else if(data.error==1001){
-                $form.data('bootstrapValidator').updateStatus('username','INVALID','callback');
-            }
-        }
-
-    }
-});
-    });
-  //重置功能
-    $("[type=rest]").on("click",function () {
-        $form.data('bootstrapValidator').resetForm();
-    })
-    
-    
-    
-    
-    
-});
->>>>>>> 3819f4e110fb35eadd93e45cb63a278fed1e340a
--- public/back/js/user.js
+++ public/back/js/user.js
@@ -1,21 +1,60 @@
 var pageSize = 5;
 var page = 1;
 
-$.ajax({
-    type: "GET",
-    url: "/user/queryUser",
-    data: {
-        page: page,
-        pageSize: pageSize
-    },
-    success: function(data) {
+function render() {
+    $.ajax({
+        type: "GET",
+        url: "/user/queryUser",
+        data: {
+            page: page,
+            pageSize: pageSize
+        },
+        success: function(data) {
 
-        console.log(data);
-        var html = template("tpl", data);
-        console.log(html);
-        $("tbody").html(html);
-        console.log('success');
+            console.log(data);
+            var html = template("tpl", data);
+            // console.log(html);
+            $("tbody").html(html);
+            // console.log('success');
+        }
 
-    }
+    })
+    $("#pagintor").bootstrapPaginator({
+        bootstrapMajorVersion: 3, //默认是2，如果是bootstrap3版本，这个参数必填
+        currentPage: page, //当前页
+        totalPages: 10, //总页数
+        size: "small", //设置控件的大小，mini, small, normal,large
+        onPageClicked: function(event, originalEvent, type, p) {
+                //为按钮绑定点击事件 page:当前点击的按钮值
+                page = p;
+                render()
+            }
+            // $('#pagintor').bootstrapPaginator(options)
+    });
+}
+render();
+$(function() {
+    $("tbody").on("click", ".btn", function() {
+        $("#banModal").modal("show");
+        var id = $(this).parent().data("id");
+        var isDelete = $(this).parent().data("isDelete");
+        $(".btn_confirm").on("click", function() {
+            $.ajax({
+                type: "POST",
+                url: "/user/updateUser",
+                data: {
+                    id: id,
+                    isDelete: isDelete,
+                },
+                success: function(data) {
+                    if (data.success) {
+                        $("#banModal").modal("hide");
+                        render();
+                    }
+                }
+            })
+        })
+    });
+
 
 })
